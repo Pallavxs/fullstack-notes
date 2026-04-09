74155ae376d81f24f1faa8f6ae3db92eab93ab7a
Pallav <programmerdata19@gmail.com>
2026-04-09 19:07:09 +0530

    Authentication yt

--- Backend/auth-yt/src/controller/userController.js
+++ Backend/auth-yt/src/controller/userController.js
@@ -69,6 +69,68 @@ export async function register(req, res) {
 
 }
 
+export async function login(req, res) {
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email })
+
+    if(!user){
+        return res.status(409).json({
+            message : "user email not valid"
+        })
+    }
+
+    // const hashPass = bcrypt.hash(password,10) 
+    
+    const isPasswordValid = bcrypt.compare(password,user.password) // automatic decrypt
+
+    if(!isPasswordValid){
+        return res.status(409).json({
+            message : "password is invalid"
+        })
+    }
+
+    const refreshToken = jwt.sign({
+        id : user._id
+    },config.JWT_SECRET,{
+        expiresIn : '7d'
+    })
+
+    const refreshTokenHash = await bcrypt.hash(refreshToken,10)
+
+    const session = await sessionModel.create({
+        user : user._id,
+        refreshTokenHash,
+        ip : req.ip,
+        userAgent : req.headers[ "user-agent" ]
+    })
+
+    const accessToken = jwt.sign({
+        id: user._id,
+        sessionId : session._id
+    }, config.JWT_SECRET,{
+        expiresIn : "15m"
+    })
+
+    res.cookie("refreshToken",refreshToken,{
+        httpOnly : true,
+        secure: true,
+        sameSite: "strict",
+        maxAge: 7 * 24 * 60 * 60 * 1000
+    })
+
+    res.status(200).json({
+        message : "user successfully registered",
+        user: {
+            username : user.username,
+            email : user.email
+        },
+        accessToken
+    })
+
+
+}
+
 export async function getMe(req, res) {
     const token = req.headers.authorization?.split(" ")[1];
 
@@ -93,18 +155,25 @@ export async function refreshToken(req, res) {
 
     if (!refreshToken) {
         return res.status(409).json({
-            message: "Refresh Token expired: "
+            message: "Refresh Token expired"
         })
     }
 
     const decoded = jwt.verify(refreshToken, config.JWT_SECRET)
 
-    const refreshTokenHash = await bcrypt.hash(refreshToken, 10)
-
-    const session = await sessionModel.findOne({
-        refreshTokenHash,
+    const sessions = await sessionModel.find({
+        user : decoded.id,
         revoked: false
     })
+
+    let session = null;
+    for(let s of sessions){
+        const isValid = await bcrypt.compare(refreshToken,s.refreshTokenHash)
+        if(isValid){
+            session = s;
+            break;
+        }
+    }
 
     if (!session) {
         return res.status(401).json({
@@ -124,7 +193,7 @@ export async function refreshToken(req, res) {
         expiresIn: '7d'
     })
 
-    const newRefreshTokenHash = await bcrypt.hash(newRefreshToken)
+    const newRefreshTokenHash = await bcrypt.hash(newRefreshToken, 10)
 
     session.refreshTokenHash = newRefreshTokenHash
     await session.save();
@@ -189,5 +258,30 @@ export async function logout(req, res) {
 
     res.status(200).json({
         message: "user is successfully logout"
+    })
+}
+
+export async function logoutAll(req, res) {
+    const refreshToken = req.cookies.refreshToken
+
+    if(!refreshToken) {
+        return res.status(400).json({
+            message : "Refresh token not found"
+        })
+    }
+
+    const decoded = jwt.verify(refreshToken, config.JWT_SECRET)
+
+    await sessionModel.updateMany({
+        user : decoded.id,
+        revoked : false
+    },{
+        revoked : true
+    })
+
+    res.clearCookie("refreshToken")
+
+    res.status(200).json({
+        message : "logout-all sucessfull"
     })
 }
--- Backend/auth-yt/src/routes/route.js
+++ Backend/auth-yt/src/routes/route.js
@@ -4,10 +4,14 @@ const userRoute = Router();
 
 userRoute.post('/register',userController.register)
 
+userRoute.post('/login', userController.login)
+
 userRoute.get('/get-me',userController.getMe)
 
 userRoute.get('/refresh-token', userController.refreshToken)
 
 userRoute.post('/logout', userController.logout)
+
+userRoute.post('/logout-all', userController.logoutAll)
 
 export default userRoute;
