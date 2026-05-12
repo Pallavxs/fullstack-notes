106128596e75c06e624efeede999e1de54e99872
Pallav <pallavshrivastava12@gmail.com>
2026-05-12 08:48:53 +0530

    Remove temp_zena_ui from git tracking

--- Backend/Snitch -19/frontend/src/app/features/auth/hooks/useAuth.js
+++ Backend/Snitch -19/frontend/src/app/features/auth/hooks/useAuth.js
@@ -7,7 +7,6 @@ export const useAuth = () => {
     const dispatch = useDispatch()
     
     async function handleRegister({ email, contact, password, fullname, isSeller = false}) {
-        
         const data = await register({email, contact, password, fullname, isSeller})
         dispatch(setUser(data.user))
         return data.user
